d938e8150040cc2c00fbd4ba3d695cd26ce84d85
moayadabukhadra <moayadabukhadra54@gmail.com>
2022-02-16 16:56:10 +0200

    class-19

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import {useState, useEffect} from 'react';
+import { Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Home/Home'
+import Home from './Components/Home/Home'
 import './App.css';
 
+import FavListitem from './Components/FavList/FavListitem';
+
+
 function App() {
+  const [favoriteList,SetFavList] = useState([]);
+
+
+const fetchData = async () => {
+  try{
+    const res = await fetch(`${process.env.REACT_APP_SERVER}/getMovies`);
+
+    const data = await res.json();
+    SetFavList(data);
+
+  }catch (error) {
+    console.log("error", error);
+  }
+
+}
+
+
+  useEffect(() => {
+    fetchData();
+  } , []);
+
   return (
-    <Home/>
+    <>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/FavList" element={<FavListitem favoriteList={favoriteList} />} />
+      </Routes>
+    </>
   );
 }
 
